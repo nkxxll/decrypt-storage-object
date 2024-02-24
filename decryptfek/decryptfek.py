3a3a1a68101f7a71e39ece67f701c41e3715008c
nkxxll <niki.nice1203@gmail.com>
2024-02-24 21:30:12 +0100

    wip(fek): decrypt fek finished not tested

--- decryptfek/decryptfek.py
+++ decryptfek/decryptfek.py
@@ -1,5 +1,27 @@
 """decrypt FEK form the encrypted object file"""
+from cryptography.hazmat.primitives.ciphers import Cipher
+from cryptography.hazmat.primitives.ciphers.algorithms import AES
+from cryptography.hazmat.primitives.ciphers.modes import ECB
 
 
-def decrypt(encrypted_fek: bytes, tsk: bytes, tsk_len: int):
-    print(f"encrypted_fek {encrypted_fek}, tsk: {tsk}, tsk_len: {tsk_len}")
+def decrypt(encrypted_fek: bytes, tsk: bytes) -> bytes:
+    """decrypt the FEK with the TSK
+
+    Decrypt the FEK with the TSK using AES in ECB mode.
+    The TSK is either 32 bytes long the length of the hmac sha256 output.
+    Args:
+        encrypted_fek (bytes): the encrypted FEK
+        tsk (bytes): the TSK
+        tsk_len (int): the length of the TSK
+    Returns:
+        bytes: the decrypted FEK
+    """
+    assert len(tsk) == 32
+    assert len(encrypted_fek) == 16
+    mode = ECB()
+    aes = AES(tsk)
+    cipher = Cipher(aes, mode)
+    decryptor = cipher.decryptor()
+    res = decryptor.update(encrypted_fek) + decryptor.finalize()
+    assert len(res) == 16
+    return res
